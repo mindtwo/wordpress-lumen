c29a119477bc521e5c97f8ef9d46c0b548d4a9da
Jonas Emde <emde@creativegroup.de>
2015-09-09 16:41:54 +0200

    Fix to psr2 codestyle

--- lumen/app/Console/Commands/RefreshBackupSettingsCommand.php
+++ lumen/app/Console/Commands/RefreshBackupSettingsCommand.php
@@ -7,48 +7,48 @@ use Illuminate\Console\Command;
 /**
  * Class RefreshBackupSettingsCommand
  */
-class RefreshBackupSettingsCommand extends Command {
+class RefreshBackupSettingsCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'wp-lumen:refresh-backup-settings';
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'wp-lumen:refresh-backup-settings';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Refresh backup settings from /install/config/install-config.json';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Refresh backup settings from /install/config/install-config.json';
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Create a new command instance.
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    /**
+     * The install components
+     *
+     * @var array
+     */
+    protected $components = [
+        \App\Console\Commands\Install\Components\ComponentBackupSettings::class,
+    ];
 
-	/**
-	 * The install components
-	 *
-	 * @var array
-	 */
-	protected $components = [
-		\App\Console\Commands\Install\Components\ComponentBackupSettings::class,
-	];
-
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire()
-	{
-		foreach($this->components as $component) {
-			(new $component)->fire();
-		}
-	}
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        foreach ($this->components as $component) {
+            (new $component)->fire();
+        }
+    }
 }
