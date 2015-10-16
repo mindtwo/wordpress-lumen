3c9082d61e32c929f0a07fb67c6021e8c74b17bc
Jonas Emde <emde@creativegroup.de>
2015-10-17 01:18:03 +0200

    Fix menu name

--- install/files/theme-default/functions/site/navigation.php
+++ install/files/theme-default/functions/site/navigation.php
@@ -7,8 +7,8 @@ add_action( 'init', 'my_custom_menus' );
 function my_custom_menus() {
 	register_nav_menus(
 		array(
-			'menu-main'   => 'Main',
-			'menu-footer' => 'Footer'
+			'menu-main'   => 'main',
+			'menu-footer' => 'footer'
 		)
 	);
 }
