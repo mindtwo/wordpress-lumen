9786ee0bc62067289184fad39ef88d7d3ace0507
Jonas Emde <emde@creativegroup.de>
2015-10-17 01:22:57 +0200

    Fix naming

--- install/files/theme-default/functions/site/navigation.php
+++ install/files/theme-default/functions/site/navigation.php
@@ -7,8 +7,8 @@ add_action( 'init', 'my_custom_menus' );
 function my_custom_menus() {
 	register_nav_menus(
 		array(
-			'menu-main'   => 'main',
-			'menu-footer' => 'footer'
+			'menu-main'   => 'Main',
+			'menu-footer' => 'Footer'
 		)
 	);
 }
