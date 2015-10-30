c97ca4ee83391be0f963ecb6c99dc7d229db1eb1
Jonas Emde <emde@creativegroup.de>
2015-10-30 01:00:28 +0100

    Change stub data

--- install/files/theme-default/config/smtp.php
+++ install/files/theme-default/config/smtp.php
@@ -1,11 +1,11 @@
 <?php
 
 return array(
-	'synnous' => array(
+	'default_mailserver' => array(
 		'PHP_MAILER_TYPE'        => 'SMTP',
-		'PHP_MAILER_HOST'        => 'mail.creativegroup.de',
+		'PHP_MAILER_HOST'        => 'mail.mindtwo.de',
 		'PHP_MAILER_SMTP_AUTH'   => true,
-		'PHP_MAILER_USERNAME'    => 'info@creativegroup.de',
+		'PHP_MAILER_USERNAME'    => 'info@mindtwo.de',
 		'PHP_MAILER_PASSWORD'    => 'xxx',
 		'PHP_MAILER_SMTP_SECURE' => 'ssl',
 		'PHP_MAILER_PORT'        => 465,
