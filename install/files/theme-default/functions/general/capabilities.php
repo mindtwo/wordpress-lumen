ec3f6a113def6ee9f77fe9b0e1950d9a395196e0
Christian Arenz <arenz@creativegroup.de>
2015-11-03 14:42:47 +0100

    Insert documentation link

--- install/files/theme-default/functions/general/capabilities.php
+++ install/files/theme-default/functions/general/capabilities.php
@@ -2,6 +2,7 @@
 
 /**
  * Adds design menu for editors
+ * https://codex.wordpress.org/Roles_and_Capabilities#Capabilities
  */
 $role_object = get_role( 'editor' );
 $role_object->add_cap( 'edit_theme_options' );
