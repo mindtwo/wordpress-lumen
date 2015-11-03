23ccb395ebd3a4d178215b74e9334ae4543c27f9
Christian Arenz <arenz@creativegroup.de>
2015-11-03 14:39:32 +0100

    Images Sizes add example comment

--- install/files/theme-default/functions/site/image-sizes.php
+++ install/files/theme-default/functions/site/image-sizes.php
@@ -2,10 +2,12 @@
 
 /**
  * Define WordPress image sizes
+ * add_image_size ( string $name, int $width, int $height, bool|array $crop = false );
+ * TODO: PHPDoc Block Comments
  */
-add_image_size( 'square', 600, 600, true );
-add_image_size( 'square-mobile', 300, 300, true );
-add_image_size( 'rectangular', 600, 300, true );
-add_image_size( 'rectangular-mobile', 400, 200, true );
-add_image_size( 'slider-image', 2545, 830, true );
-add_image_size( 'slider-image-mobile', 800, 300, array( 'center', 'center' ) );
+add_image_size ( 'square', 600, 600, true );
+add_image_size ( 'square-mobile', 300, 300, true );
+add_image_size ( 'rectangular', 600, 300, true );
+add_image_size ( 'rectangular-mobile', 400, 200, true );
+add_image_size ( 'slider-image', 2545, 830, true );
+add_image_size ( 'slider-image-mobile', 800, 300, array ( 'center', 'center' ) );
