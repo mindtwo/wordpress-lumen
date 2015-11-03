37fe0bd8a52ada51ea1ad2b12c309f762f0d1791
Christian Arenz <arenz@creativegroup.de>
2015-11-03 14:50:54 +0100

    Prevent attachment signle pages
    redirect (301) medias single page to medias parent page

---
+++ install/files/theme-default/attachment.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Redirect attachment pages to parent post or to homepage
+ */
+if ( $post->post_parent ) {
+    $page_data = get_page ( $post->post_parent );
+    if ( $page_data->post_status == 'publish' ) {
+        wp_redirect ( get_permalink ( $post->post_parent ), 301 );
+        exit;
+    }
+    else {
+        wp_redirect ( get_home_url (), 301 );
+        exit;
+    }
+}
+else {
+    wp_redirect ( get_home_url (), 301 );
+    exit;
+}
