01bdec6290d61a3343d662723c39a6d7ee74d64b
Jonas Emde <emde@creativegroup.de>
2015-11-24 22:35:55 +0100

    Fix loaded middelware

--- lumen/bootstrap/app.php
+++ lumen/bootstrap/app.php
@@ -84,10 +84,10 @@ $app->singleton(
 */
 
 $app->middleware([
-    Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-    Illuminate\Cookie\Middleware\EncryptCookies::class,
-    Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    // Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+    // Illuminate\Cookie\Middleware\EncryptCookies::class,
     Illuminate\Session\Middleware\StartSession::class,
+    Illuminate\View\Middleware\ShareErrorsFromSession::class,
     Laravel\Lumen\Http\Middleware\VerifyCsrfToken::class,
 ]);
 
