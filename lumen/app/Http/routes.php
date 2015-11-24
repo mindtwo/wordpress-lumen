e30115bf5320daea4f4287c8f9ad1ffc1c233d36
Jonas Emde <emde@creativegroup.de>
2015-11-24 21:23:24 +0100

    Add some basic routing

--- lumen/app/Http/routes.php
+++ lumen/app/Http/routes.php
@@ -1,16 +1,24 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+require_once('helpers.php');
 
-$app->get('/', function () use ($app) {
-    return $app->welcome();
-});
+// Language settings
+if(!App::runningInConsole()) {
+    setLocalInHttpEnv();
+}
+
+
+// Routs
+$app->post('/form-contact', 'ContactFormController@store');
+$app->post('/form-callback', 'CallbackFormController@store');
+
+
+// Debug mail template:
+// $app->get('/email', function() {
+//     return view('mail.contact-form', [
+//         'name' => 'John Doe',
+//         'email' =>'john@doe.com',
+//         'message' => 'Macaroon marshmallow halvah marshmallow candy canes apple pie I love. I love wypas gingerbread. Topping ice cream I love gummies jelly I love. I love topping toffee candy pie pastry. I love candy canes chocolate bar halvah pie gummi bears caramels chocolate cake. Cupcake bear claw candy canes cheesecake. Lollipop marshmallow sweet roll jujubes toffee donut wypas candy canes I love. Cupcake jelly beans danish pudding carrot cake bonbon cookie. Sugar plum cupcake muffin icing wafer tiramisu halvah pastry donut. Sweet sweet jelly beans cotton candy. I love cookie sesame snaps gummies carrot cake. Bear claw pie I love liquorice sugar plum I love gummi bears. Chocolate cake pie sugar plum candy canes bonbon chocolate bar candy marzipan. Chocolate I love bonbon carrot cake wafer macaroon faworki.',
+//         'image'=> get_wordpress_url() . 'assets/img/logo-mail.png'
+//     ]);
+// });
