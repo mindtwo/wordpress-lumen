edc1f3aa81605d2f8bf895e82da326c3834166b8
Jonas Emde <emde@creativegroup.de>
2015-11-29 20:44:15 +0100

    Add vue js form modules

---
+++ resources/assets/js/modules/FormCallback.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    template: document.querySelector('#form-callback-template'),
+
+    data: function(){
+        return {
+            inquery: { name: '', phone: '' },
+            submitted: false,
+            errors: false
+        };
+    },
+
+    methods: {
+        onSubmitForm: function(e) {
+            e.preventDefault();
+            var _inquery = this.inquery;
+            this.$http.post('/l/form-callback', _inquery).success(function(data, status, request) {
+                this.errors = false;
+                this.submitted = true;
+            }).error(function (data, status, request) {
+                var _errors = [];
+                $.each(JSON.parse(request.response), function(key, value) {
+                    _errors.push(value);
+                });
+                this.errors = _errors;
+            });
+        }
+    }
+};
