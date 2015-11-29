7769a941e522a75c4744a8d092b881139e33a64b
Jonas Emde <emde@creativegroup.de>
2015-11-29 01:27:16 +0100

    Paginate posts

--- install/files/theme-default/index.php
+++ install/files/theme-default/index.php
@@ -2,7 +2,11 @@
 
 $context = Timber::get_context();
 $context['post'] = new TimberPost();
-$args = 'post_type=post&numberposts=4&orderby=date';
+$args = array(
+	'posts_per_page' => 6,
+	'post_type' => 'post',
+	'paged' => ( get_query_var('paged') ? get_query_var('paged') : 1 )
+);
 $context['posts'] = Timber::get_posts($args);
 $context['pagination'] = Timber::get_pagination();
 Timber::render('post-types/post-list.php.twig', $context);
