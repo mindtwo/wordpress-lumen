c5365996411f543dd307feaa6e69bc593e733e3f
Jonas Emde <emde@creativegroup.de>
2016-01-15 15:27:24 +0100

    add translation helper

--- lumen/app/Http/helpers.php
+++ lumen/app/Http/helpers.php
@@ -23,7 +23,25 @@ if (! function_exists('is_wordpress')) {
 	}
 }
 
+if (! function_exists('trans')) {
+	/**
+	 * Translate the given message.
+	 *
+	 * @param  string  $id
+	 * @param  array   $parameters
+	 * @param  string  $domain
+	 * @param  string  $locale
+	 * @return string
+	 */
+	function trans($id = null, $parameters = [], $domain = 'messages', $locale = null)
+	{
+		if (is_null($id)) {
+			return app('translator');
+		}
 
+		return app('translator')->trans($id, $parameters, $domain, $locale);
+	}
+}
 
 if (! function_exists('elixir')) {
 	/**
