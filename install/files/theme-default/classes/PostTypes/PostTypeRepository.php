92f7ea91b3ecf92fbc620ba08b3bf1955f769e32
Jonas Emde <emde@creativegroup.de>
2016-01-20 19:43:58 +0100

    Add repo abstract class

---
+++ install/files/theme-default/classes/PostTypes/PostTypeRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace WpTheme\PostTypes;
+
+use Illuminate\Support\Collection;
+use Timber;
+
+/**
+ * Class PostType
+ */
+abstract class PostTypeRepository {
+
+    /**
+     * Declared in implementation
+     *
+     * @var string
+     */
+    protected $post_type = '';
+
+    /**
+     * Initialize
+     */
+    function __construct() {}
+
+    /**
+     * @param $args
+     *
+     * @return array
+     */
+    public function latest($args=null) {
+        $args = array_merge( [
+            'posts_per_page' => 6,
+            'post_type' => $this->post_type,
+            'paged' => ( get_query_var('paged') ? get_query_var('paged') : 1 )
+        ], is_array($args) ? $args : [] );
+
+        $posts = Timber::get_posts($args);
+        $pagination = Timber::get_pagination();
+        return compact('posts', 'pagination');
+    }
+
+    /**
+     * @param $post_id
+     *
+     * @return Collection
+     */
+    public function get_metas($post_id) {
+        return new Collection(get_fields($post_id));
+    }
+}
