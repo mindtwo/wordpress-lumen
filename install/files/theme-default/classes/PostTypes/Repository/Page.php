9a62813e6e7f6537e13a6cb605667b9d14e20b4f
Jonas Emde <emde@creativegroup.de>
2016-01-20 19:43:23 +0100

    Add basic repo files

---
+++ install/files/theme-default/classes/PostTypes/Repository/Page.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WpTheme\PostTypes\Repository;
+
+use WpTheme\PostTypes\PostTypeRepository;
+
+class Page extends PostTypeRepository {
+    public function __construct() {
+
+        parent::__construct();
+        $this->post_type = 'page';
+
+    }
+}
---
+++ install/files/theme-default/classes/PostTypes/Repository/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WpTheme\PostTypes\Repository;
+
+use WpTheme\PostTypes\PostTypeRepository;
+
+class Post extends PostTypeRepository {
+    public function __construct() {
+
+        parent::__construct();
+        $this->post_type = 'post';
+
+    }
+}
