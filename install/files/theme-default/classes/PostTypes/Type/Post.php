46fed2cfc8c1dd56fa7f6a215b3fc59b3fd27b0c
Jonas Emde <emde@creativegroup.de>
2016-08-22 23:48:20 +0200

    Hide some stuff from the backend

--- install/files/theme-default/classes/PostTypes/Type/Post.php
+++ install/files/theme-default/classes/PostTypes/Type/Post.php
@@ -11,8 +11,13 @@ class Post extends PostType {
 	 * Register action
 	 */
 	public function register() {
+		add_action( 'admin_menu' , [$this, 'remove_page_location_field'] );
 	}
 
+	public function remove_page_location_field() {
+		remove_meta_box( 'formatdiv', $this->post_type, 'side' );
+		remove_meta_box( 'categorydiv', $this->post_type, 'side' );
+		remove_meta_box( 'tagsdiv-post_tag', $this->post_type, 'side' );
 	}
 
 }
